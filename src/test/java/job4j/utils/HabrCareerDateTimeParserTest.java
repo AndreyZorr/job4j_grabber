3dc1605387210cc9fd5ca0a7a02dbf245667142f
Andrey <AlpatoV_A_A@mail.ru>
2023-11-20 21:06:01 +0300

    2.1. Преобразование даты [#289476 #461252]

--- src/test/java/job4j/utils/HabrCareerDateTimeParserTest.java
+++ src/test/java/job4j/utils/HabrCareerDateTimeParserTest.java
@@ -1,6 +1,8 @@
-package ru.job4j.grabber.utils;
+package job4j.utils;
 
 import org.junit.jupiter.api.Test;
+import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
+
 import java.time.LocalDateTime;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -8,7 +10,7 @@ class HabrCareerDateTimeParserTest {
     @Test
     void parseDateTimeTest() {
         HabrCareerDateTimeParser parser = new HabrCareerDateTimeParser();
-        String string = "2023-11-20T12:30:20";
+        String string = "2023-11-20T12:30:20+03:00";
         LocalDateTime exp = LocalDateTime.of(2023, 11, 20, 12, 30, 20);
         LocalDateTime rsl = parser.parse(string);
         assertThat(exp).isEqualTo(rsl);
