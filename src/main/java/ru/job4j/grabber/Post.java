c87b18f185a846a91296a5a04e96f71b818da3f4
Andrey <AlpatoV_A_A@mail.ru>
2023-11-28 23:49:57 +0300

    2.2. Модель данных - Post. [#285211 #461723]

--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -1,13 +1,18 @@
-package ru.job4j.grabber.utils;
+package ru.job4j.grabber;
 
 import java.time.LocalDateTime;
 import java.util.Objects;
 
 public class Post {
+
     private int id;
+
     private String title;
+
     private String link;
+
     private String description;
+
     private LocalDateTime created;
 
     public Post(int id, String title, String link, String description, LocalDateTime created) {
