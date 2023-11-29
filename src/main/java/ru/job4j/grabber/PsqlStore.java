62a91659510b6a0ff4bad20625ba5509fc8721be
Andrey <AlpatoV_A_A@mail.ru>
2023-11-29 13:55:10 +0300

    5. PsqlStore [#285209]

---
+++ src/main/java/ru/job4j/grabber/PsqlStore.java
@@ -0,0 +1,99 @@
+package ru.job4j.grabber;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class PsqlStore implements Store {
+
+    private Connection cnn;
+
+    public PsqlStore(Properties cfg) {
+        try {
+            Class.forName(cfg.getProperty("jdbc.driver"));
+            cnn = DriverManager.getConnection(
+                    cfg.getProperty("url"),
+                    cfg.getProperty("username"),
+                    cfg.getProperty("password")
+            );
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Override
+    public void save(Post post) {
+        try (PreparedStatement ps = cnn.prepareStatement(
+                "INSERT INTO post(name, text, link, created) VALUES (?, ?, ?, ?) ON CONFLICT (link) DO NOTHING",
+                Statement.RETURN_GENERATED_KEYS)) {
+            ps.setString(1, post.getTitle());
+            ps.setString(2, post.getLink());
+            ps.setString(3, post.getDescription());
+            ps.setTimestamp(4, Timestamp.valueOf(post.getCreated()));
+            ps.executeUpdate();
+            try (ResultSet resultSet = ps.getGeneratedKeys()) {
+                while (resultSet.next()) {
+                    post.setId(resultSet.getInt("id"));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Post> getAll() {
+        List<Post> list = new ArrayList<>();
+        try (PreparedStatement statement = cnn.prepareStatement("SELECT * FROM post")) {
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    list.add(creat(resultSet));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+        return list;
+    }
+
+    @Override
+    public Post findById(int id) {
+        Post post = new Post();
+        try (PreparedStatement ps = cnn.prepareStatement("SELECT * FROM post WHERE id = ?")) {
+            ps.setInt(1, id);
+            try (ResultSet resultSet = ps.executeQuery()) {
+                while (resultSet.next()) {
+                    post.setId(resultSet.getInt("id"));
+                    post.setTitle(resultSet.getString("name"));
+                    post.setLink(resultSet.getString("link"));
+                    post.setDescription(resultSet.getString("text"));
+                    post.setCreated(resultSet.getTimestamp("created").toLocalDateTime());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return post;
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (cnn != null) {
+            cnn.close();
+        }
+    }
+
+    private Post creat(ResultSet resultSet) {
+        try {
+            return new Post(resultSet.getInt("id"),
+                    resultSet.getString("name"),
+                    resultSet.getString("link"),
+                    resultSet.getString("text"),
+                    resultSet.getTimestamp("created").toLocalDateTime());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
