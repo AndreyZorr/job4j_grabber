2358dbe49d19c62c14efe84820abb023de3c6fc6
Andrey <AlpatoV_A_A@mail.ru>
2024-01-24 11:47:35 +0300

    Принципы Kiss, Dry и Yagni

---
+++ src/main/java/ru/job4j/condition/Maximum.java
@@ -0,0 +1,37 @@
+package ru.job4j.condition;
+
+//KISS
+public class Maximum {
+    public static int getMax(int left, int right) {
+        return left > right ? left : right;
+    }
+
+    public static int max(int first, int second, int third, int forth) {
+        return getMax(
+                getMax(first, second),
+                getMax(third, forth)
+        );
+    }
+
+    public static void main(String[] args) {
+        System.out.println(max(4000, 3,  10, 100));
+    }
+
+    public static int getMax(int first, int second, int third, int forth) {
+        int result = forth;
+        if (first > second) {
+            if (first > third) {
+                if (first > forth) {
+                    result = third;
+                }
+            }
+        } else if (second > third) {
+            if (second > forth) {
+                result = first;
+            }
+        } else if (third > forth) {
+            result = second;
+        }
+        return result;
+    }
+}
